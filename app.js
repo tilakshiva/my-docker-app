9306e62e6645e0f7ca73b95ead310ee201485f9c
Ubuntu <ubuntu@ip-172-31-42-210.ec2.internal>
2019-02-10 00:52:53 +0000

    added supertest

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+var express = require('express')
+var app = express()
 
-var express = require('express'),
-    app = express();
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-app.set('views', 'views');
-app.set('view engine', 'pug');
+//Launch listening server on port 8080
+app.listen(8080, function () {
+  console.log('App listening on port 8080!')
+})
 
-app.get('/', function(req, res) {
-    res.send('all is working well');
-});
-
-app.listen(8080);
 module.exports.getApp = app;
